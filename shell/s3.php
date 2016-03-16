e8cb0736f55593fd377034d1a700c21df54ca1d6
Thai Phan <thai@outlook.com>
2016-03-17 01:52:13 +1100

    Add shell script to import files into S3
    If you have heaps of images then trying to sync from the admin panel might cause
    the export to timeo ut. I've added a shell script so you can run safely from the
    server.

---
+++ shell/s3.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'abstract.php';
+
+class Arkade_Shell_S3 extends Mage_Shell_Abstract
+{
+    public function run()
+    {
+        /** @var Mage_Core_Helper_File_Storage $helper */
+        $helper = Mage::helper('core/file_storage');
+
+        // Stop S3 from syncing to itself
+        if (Arkade_S3_Model_Core_File_Storage::STORAGE_MEDIA_S3 == $helper->getCurrentStorageCode()) {
+            return $this;
+        }
+
+        /** @var Mage_Core_Model_File_Storage_File|Mage_Core_Model_File_Storage_Database $sourceModel */
+        $sourceModel = $helper->getStorageModel();
+
+        /** @var Arkade_S3_Model_Core_File_Storage_S3 $destinationModel */
+        $destinationModel = $helper->getStorageModel(Arkade_S3_Model_Core_File_Storage::STORAGE_MEDIA_S3);
+
+        $offset = 0;
+        while (($files = $sourceModel->exportFiles($offset, 1)) !== false) {
+            foreach ($files as $file) {
+                echo $file['directory'] . '/' . $file['filename'] . "\n";
+            }
+            if (!$this->getArgs('dry-run')) {
+                $destinationModel->importFiles($files);
+            }
+            $offset += count($files);
+        }
+        unset($files);
+
+        if (!$this->getArgs('dry-run')) {
+            Mage::getConfig()->saveConfig('system/media_storage_configuration/media_storage', 2);
+        }
+
+        return $this;
+    }
+}
+
+$shell = new Arkade_Shell_S3();
+$shell->run();
