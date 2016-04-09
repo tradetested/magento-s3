970c26bddcfebc5890a24ba87305cc1072d3580d
Thai Phan <thai@outlook.com>
2016-04-09 19:51:54 +1000

    Functionise repeated code

--- app/code/community/Arkade/S3/Model/Cms/Wysiwyg/Images/Storage.php
+++ app/code/community/Arkade/S3/Model/Cms/Wysiwyg/Images/Storage.php
@@ -2,12 +2,13 @@
 
 class Arkade_S3_Model_Cms_Wysiwyg_Images_Storage extends Mage_Cms_Model_Wysiwyg_Images_Storage
 {
+    private $s3Helper = null;
+
     public function getDirsCollection($path)
     {
-        /** @var Arkade_S3_Helper_Core_File_Storage_Database $helper */
-        $helper = Mage::helper('arkade_s3/core_file_storage_database');
-        if ($helper->checkS3Usage()) {
-            $storageModel = $helper->getStorageDatabaseModel();
+        if ($this->getS3Helper()->checkS3Usage()) {
+            /** @var Arkade_S3_Model_Core_File_Storage_S3 $storageModel */
+            $storageModel = $this->getS3Helper()->getStorageDatabaseModel();
             $subdirectories = $storageModel->getSubdirectories($path);
 
             foreach ($subdirectories as $directory) {
@@ -22,10 +23,9 @@ class Arkade_S3_Model_Cms_Wysiwyg_Images_Storage extends Mage_Cms_Model_Wysiwyg_
 
     public function getFilesCollection($path, $type = null)
     {
-        /** @var Arkade_S3_Helper_Core_File_Storage_Database $helper */
-        $helper = Mage::helper('arkade_s3/core_file_storage_database');
-        if ($helper->checkS3Usage()) {
-            $storageModel = $helper->getStorageDatabaseModel();
+        if ($this->getS3Helper()->checkS3Usage()) {
+            /** @var Arkade_S3_Model_Core_File_Storage_S3 $storageModel */
+            $storageModel = $this->getS3Helper()->getStorageDatabaseModel();
             $files = $storageModel->getDirectoryFiles($path);
 
             /** @var Mage_Core_Model_File_Storage_File $fileStorageModel */
@@ -35,5 +35,16 @@ class Arkade_S3_Model_Cms_Wysiwyg_Images_Storage extends Mage_Cms_Model_Wysiwyg_
             }
         }
         return parent::getFilesCollection($path, $type);
+    }
+
+    /**
+     * @return Arkade_S3_Helper_Core_File_Storage_Database
+     */
+    protected function getS3Helper()
+    {
+        if (is_null($this->s3Helper)) {
+            $this->s3Helper = Mage::helper('arkade_s3/core_file_storage_database');
+        }
+        return $this->s3Helper;
     }
 }
